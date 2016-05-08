67e85cbd05b5ac040420daec372b389d328a4ebb
tonykung <tonykung06@gmail.com>
2016-05-08 19:24:45 +0800

    trying out exceptino handlings

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+Error.stackTraceLimit = Infinity;
+process.on('uncaughtException', function(e) {
+	console.log('got an uncaught exception, this might because you have not handled error in callbacks and an exception bubbles all the way back to the event loop. Node.js handles such exceptions by printing the stack trace to stderr and exiting. Crash the app then and please fix the bug.', e.stack);
+	throw e; //the exception stack trace will return to the frontend when the env variable NODE_ENV is not equal to production.
+});
+
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var pg = require('pg');
@@ -16,7 +22,6 @@ app.get('/', function(request, response) {
 });
 
 app.get('/cool', function(request, response) {
-	throw "OMG cool is fxxked up";
   response.send(cool());
 });
 
@@ -29,6 +34,7 @@ app.get('/times', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
+	request.testing();
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
@@ -38,10 +44,14 @@ app.get('/db', function (request, response) {
        { response.render('pages/db', {results: result.rows} ); }
     });
   });
-})
+});
+
+//custom catch-all exception handler
+app.use(function errorHandler(err, req, res, next) {
+	console.log('catch all exceptions except those bubbling up to the event loop from callbacks. Then continue on by leaving the exception to the Express in-built handler.');
+	next(err);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
